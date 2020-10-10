3ee90efe8d295ace82bc336c98952a022d616eb7
NoorieKhan <nooriezak@gmail.com>
2020-10-10 22:52:40 +0530

    changed force

--- sketch.js
+++ sketch.js
@@ -9,30 +9,23 @@ var world;
 
 
 function setup() {
-	createCanvas(1000, 500);
+	createCanvas(1600, 700);
 	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
-	dustbinObj=new dustbin(890,450);
-	paperObject=new paper(70,450,70);
-	groundObject=new ground(width/2,470,width,20);
+	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
 	//Create a Ground
 	
 
-	/*var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});*/
+	
 
 	Engine.run(engine);
-	//Render.run(render);
+	Render.run(render);
   
 }
 
@@ -42,10 +35,13 @@ function draw() {
   background(230);
  
   
- 
+  paperObject.display();
   groundObject.display();
   dustbinObj.display();
-  paperObject.display();
+
+  
+  
+ 
   
   
  
@@ -54,12 +50,8 @@ function draw() {
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:110,y:-145});
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
     
   	}
 }
-
-
-
-
-
